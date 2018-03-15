2faaffa5a91611b4b7412a56be3c8511bec2bbda
Samantha Marshall <me@samdmarshall.com>
2018-03-14 23:18:07 -0400

    i don't know how to write javascript, so i'll just delete code until it work

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,9 +1,6 @@
 const {danger, warn} = require('danger')
-const {yarn} = require('danger-plugin-yarn')
 
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
 		warn('Please include a description of your PR changes.');
 }
-
-schedule(yarn())
