79b8bf6c4c6c1b7ab9c19fa6265494db3e461224
Samantha Marshall <me@samdmarshall.com>
2018-03-18 23:43:08 -0400

    adding more work to the dangerfile

--- Dangerfile.js
+++ Dangerfile.js
@@ -4,3 +4,20 @@ const {danger, warn} = require('danger')
 if (danger.github.pr.body.length < 10) {
 		warn('Please include a description of your PR changes.');
 }
+
+// Make sure that the yarn.lock and package.json don't get out of sync
+const packageChanged = includes(danger.git.modified_files, 'package.json');
+const lockfileChanged = includes(danger.git.modified_files, 'yarn.lock');
+if (packageChanged && !lockfileChanged) {
+	const message = 'Changes were made to package.json, but not to yarn.lock';
+	const idea = 'Perhaps you need to run `yarn install`?';
+	warn(`${message} - <i>${idea}</i>`);
+}
+
+// Add a CHANGELOG entry for app changes
+const hasChangelog = includes(danger.git.modified_files, "changelog.md")
+const isTrivial = contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
+
+if (!hasChangelog && !isTrivial) {
+		warn("Please add a changelog entry for your changes.")
+}
