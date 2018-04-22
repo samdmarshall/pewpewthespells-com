1ee3b6439cc901a29a8162c5d4aee3470947fccb
Samantha Demi <me@samdmarshall.com>
2018-04-22 10:54:03 -0700

    adding more to get danger to hopefully work?

--- code-dangerfile.ts
+++ code-dangerfile.ts
@@ -22,5 +22,5 @@ if (!hasChangelog) {
 }
 
 // check that all of the documents are spell-check
-//import spellcheck from 'danger-plugin-spellcheck'
-//spellcheck()
+import spellcheck from 'danger-plugin-spellcheck'
+spellcheck()
